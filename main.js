34a04b5d948d19c7a4f2b9d437528de5dd1e8f30
zniguefmohamed <mohamedzniguef@gmail.com>
2021-12-15 16:29:01 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 30) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide up
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
+
+  //toggle menu
+  $(".navbar .menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".navbar .menu-btn i ").toggleClass("active");
+  });
+
+  // typing animation
+  var typed = new Typed(".typing-2", {
+    strings: ["Web Developer.", "JavaScript Developer.", "Python Developer.", "Freelancer."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  })
+
+  var typed = new Typed(".typing", {
+    strings: ["Web Developer", "JavaScript Developer", "Python Developer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  })
+
+  // owl-carousel
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoPlayTimeOut: 2000,
+    autoPlayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+
+      600: {
+        items: 3,
+      },
+
+      100: {
+        items: 1,
+      },
+    },
+  });
+});
