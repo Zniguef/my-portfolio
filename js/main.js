13449ed13370482938bce852c1cdfa093809d4f8
zniguefmohamed <mohamedzniguef@gmail.com>
2022-10-01 17:27:54 +0100

    fix responsive & add new skill

--- js/main.js
+++ js/main.js
@@ -26,22 +26,14 @@ $(document).ready(function () {
 
   // typing animation
   var typed = new Typed(".typing-2", {
-    strings: [
-      "Web Developer.",
-      "JavaScript Developer.",
-      "Freelancer.",
-    ],
+    strings: ["Web Developer.", "JavaScript Developer.", "Freelancer."],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing", {
-    strings: [
-      "Web Developer.",
-      "JavaScript Developer.",
-      "Freelancer.",
-    ],
+    strings: ["Web Developer.", "JavaScript Developer."],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
